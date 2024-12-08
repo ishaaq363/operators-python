8d2a614125cb8bc9611e5da6f2d42b680f4ca0b3
ishaaq363 <imatovu383@gmail.com>
2024-12-08 18:48:19 +0200

    operations

---
+++ main.py
@@ -0,0 +1,24 @@
+a = int(input("Enter your first number"))
+b = int(input("Enter your last number"))
+c = a+b
+print(c)
+
+name = "Is-haaq"
+age = 34
+
+is_client = False
+weight = 65.12
+print(type(name))
+
+number = "2"
+print(type(number))
+ishaaq = int(number)
+print(type(ishaaq))
+
+a = 2
+b = 4
+c = b
+b = a
+c = a
+print("Value of a is how",a)
+print("Value of b is how",b)
